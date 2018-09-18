730523a1ac37d4b256c406654a00dfbe3b65af53
Jorge Ribeiro <joorgemelo@gmail.com>
2018-09-18 13:15:11 -0300

    modified:   resources/views/dicas.blade.php

--- resources/views/dicas.blade.php
+++ resources/views/dicas.blade.php
@@ -2,7 +2,21 @@
 
 <body>
     <section class="section">
-        Building
+        <div class="tile is-ancestor">
+            <div class="tile is-4 is-vertical is-parent">
+                <div class="tile is-child box">
+                    <p class="title">One</p>
+                </div>
+                <div class="tile is-child box">
+                    <p class="title">Two</p>
+                </div>
+            </div>
+            <div class="tile is-parent">
+                <div class="tile is-child box">
+                    <p class="title">Three</p>
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 </html>
