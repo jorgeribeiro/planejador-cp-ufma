bc27c7ba58a57b38fe925668bf142363cccea51a
Jorge Ribeiro <joorgemelo@gmail.com>
2018-09-19 10:06:48 -0300

    modified:   database/database.sqlite 	modified:   resources/views/dicas.blade.php

--- resources/views/dicas.blade.php
+++ resources/views/dicas.blade.php
@@ -5,15 +5,37 @@
         <div class="tile is-ancestor">
             <div class="tile is-4 is-vertical is-parent">
                 <div class="tile is-child box">
-                    <p class="title">One</p>
+                    <p class="title">Quantidade de disciplinas</p>
+                    <p class="subtitle">Tente fazer no mínimo 6 disciplinas por semestre. 
+                        Algumas disciplinas são bem fáceis e não exigem tanto, podendo colocar até mais que 6.
+                        Fazendo 6 por semestre, é possível até se graduar em 4 anos. (Não reprovando, claro)
+                    </p>
                 </div>
                 <div class="tile is-child box">
-                    <p class="title">Two</p>
+                    <p class="title">Horas complementares</p>
+                    <p class="subtitle">Não deixe pra participar de congressos, eventos e feiras perto de se formar.
+                        O que aparecer pra participar, participe. Iniciação Científica, PET e semelhantes garantem boas horas.
+                    </p>
                 </div>
             </div>
             <div class="tile is-parent">
                 <div class="tile is-child box">
-                    <p class="title">Three</p>
+                    <p class="title">Disciplinas do Grupo II</p>
+                    <p class="subtitle">Escolha corretamente para não fazer horas a mais desnecessariamente. 
+                        São 225h exigidas, o que equivale a 60h * 3 + 45h.
+                        Logo, no primeiro período todos são matriculados em Inglês I (45h), 
+                        sobrando apenas 3 disciplinas de 60h para fazer. Evite disciplinas de 45h.
+                    </p>                    
+                </div>
+            </div>
+            <div class="tile is-parent">
+                <div class="tile is-child box">
+                    <p class="title">Estágio</p>
+                    <p class="subtitle">Quando estiver no 5º período já busque algum estágio (que seja válido, claro).
+                        Não deixe para cumprir as horas do estágio no último período. 
+                        É possível estagiar na própria UFMA, no NTI e até em laboratórios,
+                        basta conversar com os professores responsáveis e se informar.
+                    </p>                    
                 </div>
             </div>
         </div>
